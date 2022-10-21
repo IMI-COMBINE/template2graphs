7b3c74cc17fbfc7a9f26cf5fdcddd7112c07ad0d
YojanaGadiya <45199062+YojanaGadiya@users.noreply.github.com>
2022-10-21 14:04:25 +0200

    update graph code

--- src/main.py
+++ src/main.py
@@ -2,8 +2,13 @@
 
 """Main file for running the KG."""
 
+import os
 import pandas as pd
+from datetime import datetime
+from py2neo import Graph
 
+from src.nodes import add_nodes
+from src.relations import add_relations
 from src.data_preprocessing import harmonize_data
 
 pd.set_option('display.max_columns', None)
@@ -11,11 +16,10 @@ pd.set_option('display.max_columns', None)
 DATA_DIR = '../data'
 
 
-def get_data():
+def get_data() -> pd.DataFrame:
     df = pd.read_csv(f'{DATA_DIR}/dummy_data.tsv', sep='\t', skiprows=4)
     df.drop([
         'Variable',
-        'BACTERIAL_STRAIN_SITE_REF',
         '#NA (not applicable)',
         '#NA (not applicable).1',
         'StdDev'
@@ -23,12 +27,57 @@ def get_data():
 
     df = harmonize_data(df)
 
+    return df
 
 
+def export_triples(
+    graph: Graph
+):
+    """Exporting triples of the graph"""
 
-def create_graph():
-    pass
+    DATE = datetime.today().strftime('%d_%b_%Y')
+    t = graph.run(
+        'Match (n)-[r]-(m) Return n.name, n.curie, type(r), m.name, m.curie'
+    ).to_data_frame()
+
+    graph_dir = f'{DATA_DIR}/graph'
+    os.makedirs(graph_dir, exist_ok=True)
+    return t.to_csv(f'{graph_dir}/base_triples-{DATE}.tsv', sep='\t', index=False)
+
+
+def create_graph(data_df: pd.DataFrame):
+    """Main function to create and populate the graph."""
+
+    # TODO: FixMe
+    graph_url = 'bolt://localhost:7687'
+    graph_admin_name = 'yojana_test'
+    graph_pass = 'fraunhofer22'
+
+    graph = Graph(
+        graph_url,
+        auth=(graph_admin_name, graph_pass),
+    )
+    tx = graph.begin()
+    graph.delete_all()  # delete existing data
+
+    # Creating nodes
+    node_map = add_nodes(
+        tx=tx,
+        df=data_df
+    )
+
+    # Creating edges
+    # add_relations(
+    #     df=data_df,
+    #     node_mapping_dict=node_map,
+    #     tx=tx
+    # )
+
+    graph.commit(tx)
+
+    # export_triples(graph)
 
 
 if __name__ == '__main__':
-    get_data()
+    edge_data = get_data()
+    create_graph(data_df=edge_data)
